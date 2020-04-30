ad71b880a0690366414981991265f8320e6ee49f
jayanathWLY <jkarunarat@wiley.com>
2020-04-30 09:48:17 +0530

    Added method to main class to call add method.

--- src/main/java/com/wiley/main/Application.java
+++ src/main/java/com/wiley/main/Application.java
@@ -1,7 +1,11 @@
 package com.wiley.main;
 
+import com.wiley.functions.Basic;
+
 public class Application {
 
     public static void main(String[] args) {
+        Basic basic = new Basic();
+        System.out.println(basic.add(10, 20));
     }
 }
