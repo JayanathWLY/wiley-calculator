2d29dbc26ebd19c01a47f189a5ee9f4956cb4eb6
jayanathWLY <jkarunarat@wiley.com>
2020-04-30 08:41:39 +0530

    Updated main method to execute add.

--- src/main/java/com/wiley/main/Application.java
+++ src/main/java/com/wiley/main/Application.java
@@ -1,7 +1,13 @@
 package com.wiley.main;
 
+import com.wiley.functions.Basic;
+
 public class Application {
 
     public static void main(String[] args) {
+        Basic basic = new Basic();
+        long input1 = Long.parseLong(args[0]);
+        long input2 = Long.parseLong(args[1]);
+        System.out.println(basic.add(input1, input2));
     }
 }
